1b7ba90d74b4e2119b47db1c88af754e4587a3e7
Bridger Tower <bridgertower@gmail.com>
2023-10-17 16:21:47 -0600

    udpate

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $query = new WP_Query(array('post_type' => 'post'));
 if ($query->have_posts()) {
     while ($query->have_posts()) {
         $query->the_post();
-        echo get_the_title();
+        echo "<br>" + get_the_title();
     }
 } else {
     echo 'No posts found';
