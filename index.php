bbeadaa33bc8aa54171f3f39284ff6520c0eb2a1
Bridger Tower <bridgertower@gmail.com>
2023-10-17 16:20:04 -0600

    h2 -> p

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $query = new WP_Query(array('post_type' => 'post'));
 if ($query->have_posts()) {
     while ($query->have_posts()) {
         $query->the_post();
-        echo '<h2>' . get_the_title() . '</h2>';
+        echo '<p>' . get_the_title() . '</p>';
     }
 } else {
     echo 'No posts found';
