199f67462994e352b04653366e424cdf48440a9b
Bridger Tower <bridgertower@gmail.com>
2023-10-17 16:09:54 -0600

    add screeshot and homepage

--- index.php
+++ index.php
@@ -2,3 +2,15 @@
 // Silence is golden.
 
 echo "Hello World!";
+$query = new WP_Query(array('post_type' => 'post'));
+
+if ($query->have_posts()) {
+    while ($query->have_posts()) {
+        $query->the_post();
+        echo '<h2>' . get_the_title() . '</h2>';
+    }
+} else {
+    echo 'No posts found';
+}
+
+wp_reset_postdata();
