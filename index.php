a0beb5002fb9e7099d0ba2d2b15dce502a8a70f2
Bridger Tower <57158102+brijr@users.noreply.github.com>
2024-06-04 16:36:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 
 echo "<p>Hello World! Use the Rest API to access the posts: /wp-json/wp/v2/posts</p>";
 
-echo "<a href="/admin">Login to WP Admin</a>";
+echo "<a href='/admin'>Login to WP Admin</a>";
